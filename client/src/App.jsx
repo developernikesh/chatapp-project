11240fe59783d1c1c7aa007c0b2c417fd36f00f8
Nikesh Sharma <eagernikesh@gmail.com>
2023-07-19 22:39:17 +0545

    first

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,49 +10,51 @@ import './App.css';
 
 const cookies = new Cookies();
 
-const apiKey = 'qgtk9ttyha7j';
-const authToken = cookies.get("token");
+const apiKey = 'tuprs2yb4eqz';
+const authToken = cookies.get('token');
 
 const client = StreamChat.getInstance(apiKey);
 
-if(authToken) {
-    client.connectUser({
-        id: cookies.get('userId'),
-        name: cookies.get('username'),
-        fullName: cookies.get('fullName'),
-        image: cookies.get('avatarURL'),
-        hashedPassword: cookies.get('hashedPassword'),
-        phoneNumber: cookies.get('phoneNumber'),
-    }, authToken)
+if (authToken) {
+  client.connectUser(
+    {
+      id: cookies.get('userId'),
+      name: cookies.get('username'),
+      fullName: cookies.get('fullName'),
+      image: cookies.get('avatarURL'),
+      hashedPassword: cookies.get('hashedPassword'),
+      phoneNumber: cookies.get('phoneNumber'),
+    },
+    authToken
+  );
 }
-
 
 const App = () => {
-    const [createType, setCreateType] = useState('');
-    const [isCreating, setIsCreating] = useState(false);
-    const [isEditing, setIsEditing] = useState(false);
+  const [createType, setCreateType] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-    if(!authToken) return <Auth />
+  if (!authToken) return <Auth />;
 
-    return (
-        <div className="app__wrapper">
-            <Chat client={client} theme="team light">
-                <ChannelListContainer 
-                    isCreating={isCreating}
-                    setIsCreating={setIsCreating}
-                    setCreateType={setCreateType}
-                    setIsEditing={setIsEditing}
-                />
-                <ChannelContainer 
-                    isCreating={isCreating}
-                    setIsCreating={setIsCreating}
-                    isEditing={isEditing}
-                    setIsEditing={setIsEditing}
-                    createType={createType}
-                />
-            </Chat>
-        </div>
-    );
-}
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
+        />
+      </Chat>
+    </div>
+  );
+};
 
 export default App;
